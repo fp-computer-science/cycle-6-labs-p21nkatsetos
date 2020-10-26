d88a3a01540a29ba6f210f108faa9e10a1c2124e
Nicholas K <p21nkatsetos@fairfieldprep.org>
2020-10-26 09:20:18 -0400

    Add 2-1

---
+++ lab1-1.py
@@ -0,0 +1,16 @@
+# Author: NJK (AMDG) 10/23/20
+
+# int(a)
+# I think this is going to run as a syntax error; it is a name error.
+
+# int('a')
+# I think this is going to run as a value error; it is a value error.
+
+# 'a' + 2
+# I think it's going to be a type error; it is a type error.
+
+# import date
+# I think it's going to be module not found error; it is a module not found error.
+
+# print("I'm a happy camper!)
+# I think it's going to be a name error; it is actually a syntax error.
---
+++ lab2-1.py
@@ -0,0 +1,8 @@
+# Author: NJK (AMDG) 10/26/20
+
+import random
+print(random.randint(1, 100))
+
+random.seed(7)
+print(random.randrange(0, 101, 2))
+
