4fbae7f00f80f95e9a8bd883a87030ea32700998
Nicholas K <p21nkatsetos@fairfieldprep.org>
2020-10-27 13:06:31 -0400

    Submit cycle 6 day 3 labs

---
+++ lab3-1.py
@@ -0,0 +1,14 @@
+# Author: NJK (AMDG) 10/27/20
+
+import math as m
+
+print(m.ceil(7.15))
+print(m.floor(19.156))
+print(m.fabs(-42))
+print(m.pow(2, 3))
+print(m.log10(100))
+print(m.sqrt(9))
+print(m.isqrt(2))
+print(m.factorial(5))
+print(m.gcd(111111111, 12321))
+print(m.sin(0))
